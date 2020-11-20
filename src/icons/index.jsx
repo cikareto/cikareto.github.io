f479cdb0b4cd828dd79660aa289ec596f82c5c2a
cikareto <salisa_cct@hotmail.com>
2020-11-20 20:49:26 +0700

    add social icons

---
+++ src/icons/index.jsx
@@ -0,0 +1,6 @@
+import TwitterIcon from "./twitter";
+import GithubIcon from "./github";
+import LinkedInIcon from "./linkedin";
+import MediumIcon from "./medium";
+
+export { TwitterIcon, GithubIcon, LinkedInIcon, MediumIcon };
