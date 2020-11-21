0566bf8998b3d27a7dcf9aeec0d83adf5a513407
cikareto <salisa_cct@hotmail.com>
2020-11-21 22:36:01 +0700

    try to set center at object but failed 🤬

--- src/components/Sphere/index.jsx
+++ src/components/Sphere/index.jsx
@@ -4,15 +4,16 @@ import React, { useRef } from 'react'
 
 import { Canvas, useThree, useResource } from 'react-three-fiber'
 import { OrbitControls } from "drei";
+import * as cx from 'classnames'
 
 import './style.css'
 
-const Scene = () => {
+const Scene = ({position = [0, 0, 0], size = 1, opacity = 1}) => {
   return (
     <>
-      <mesh visible position={[0, 0, 0]} rotation={[0.5, 0, 0]}>
-        <sphereGeometry attach="geometry" args={[2.5, 5, 4]} />
-        <meshStandardMaterial wireframe attach="material" color={0xffffff} transparent opacity={0.6} />
+      <mesh visible position={position} rotation={[0.5, 0, 0]} >
+        <sphereGeometry attach="geometry" args={[size, 5, 4]} />
+        <meshStandardMaterial wireframe attach="material" color={0xffffff} transparent opacity={opacity} />
       </mesh>
     </>
   )
@@ -25,28 +26,34 @@ const Controls = () => {
 
   const args = [camera, gl.domElement];
 
-  return <OrbitControls ref={object} autoRotate {...{ ref, args }} />;
+  return (
+    <OrbitControls ref={object} 
+       autoRotate
+       {...{ ref, args }} 
+    />
+  );
 };
 
 const Light = () => {
   return (
     <> 
-      <pointLight position={[10, 10, 20]} color="#2E9AE5" intensity={0.9} />
+      <spotLight position={[0, 0, 10]} angle={0.15} penumbra={1} color="#2E9AE5"/>
+      <pointLight position={[0, 0, 20]} color="#2E9AE5" intensity={0.9} />
       <ambientLight color="#2C76FF" intensity={0.8} />
     </>
   )
 }
 
 
-const Sphere = () => {
+const CanvasSphere = ({className, position, size, opacity}) => {
   return (
-    <Canvas className="canvas-sphere">
-    <Light />
-    <Scene />
-    <Controls />
-  </Canvas>
+    <Canvas className={cx("canvas-sphere", className)}>
+      <Scene position={position} size={size} opacity={opacity} />
+      <Light />
+      <Controls target={position}/>
+    </Canvas>
   )
 }
 
-export default Sphere
+export default CanvasSphere
 
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,17 +1,17 @@
 import Intro from "./Intro";
 import Sphere from "../../components/Sphere";
 
-import './index.css'
+import "./index.css";
 
 const Home = () => {
   return (
     <div className="home">
       <Intro />
       <div className="home__spheres">
-        <Sphere />
+        <Sphere position={[0,0,0]} size={2.5} opacity={0.6}/>
       </div>
     </div>
   );
 };
 
-export default Home
+export default Home;
