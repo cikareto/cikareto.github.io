c30de717a416e4795f210f331c4793e1a047fe3c
cikareto <salisa_cct@hotmail.com>
2020-11-29 20:40:59 +0700

    chore: use cdn for static files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 const withCSS = require('@zeit/next-css')
 const withImages = require('next-images')
 
-module.exports = withImages(
-  withCSS({
-    target: 'serverless'
-  })
-)
+const config = {
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/cikareto/cikareto.github.io/gh-pages/' : '/',
+};
+
+module.exports = withCSS(withImages(config));
