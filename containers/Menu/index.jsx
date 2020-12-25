e1d4de75a177b1ccc575f8ab85007b0f7485de7e
cikareto <salisa_cct@hotmail.com>
2020-12-25 18:29:46 +0700

    active nav

--- containers/Menu/index.jsx
+++ containers/Menu/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import cx from 'classnames'
 import { HomeIcon, AboutIcon, SkillIcon, ContactIcon } from "../../icons";
 
 import './menu.css'
 
 const Menu = () => {
+
+  const router = useRouter()
+
   return (
     <div className="menu">
-      <Link exact href='/'><a><HomeIcon /></a></Link>
-      <Link exact href='/about'><a><AboutIcon /></a></Link>
+      <Link exact href='/'><a className={cx({'active': router.pathname === '/'})}><HomeIcon /></a></Link>
+      <Link exact href='/about'><a className={cx({'active': router.pathname === '/about'})}><AboutIcon /></a></Link>
       <Link exact href='/skill'><a className="link--disabled"><SkillIcon /></a></Link>
       <Link exact href='/contact'><a className="link--disabled"><ContactIcon /></a></Link>
     </div>
